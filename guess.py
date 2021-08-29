63dba72ab5b88b65f8adf3aff6579763108e1bc0
Lakshya <lakshya7878@gmail.com>
2021-08-29 23:35:16 +0530

    Code added

--- guess.py
+++ guess.py
@@ -1,1 +1,13 @@
-print("hello lakshya github")
+import random
+value = random.randint(1,100)
+while True :
+    guess  = int(input("Guess the number : "))
+    if guess<value :
+        print("Try a bit higher")
+    elif guess>value :
+        print("try a little lower")
+    else :
+        print("congratulations you won!!!")
+        break
+
+
